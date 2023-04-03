c3fa674dc9aadfd504fc8e7d856337a8c645f0f9
SHAIK-MUNEERUDDIN <120248240+SHAIK-MUNEERUDDIN@users.noreply.github.com>
2023-04-03 07:49:38 +0530

    fix-javascript for hamburger

--- hamburger.js
+++ hamburger.js
@@ -1,13 +1,13 @@
-let ham = document.getElementById('hamburger');
+let ham = document.getElementById('menu-toggle');
 let container = document.getElementsByClassName('cointner-1')[0];
 let navtext = document.getElementById('navtext');
 
-ham.addEventListener('click',()=>{
-    if (navtext.style.display == 'block'){
+ham.addEventListener('click', () => {
+    if (navtext.style.display == 'block') {
         navtext.style.display = 'none';
         container.style.height = '4vh';
     }
-    else{
+    else {
         container.style.height = '45vh';
         navtext.style.display = 'block';
     }
